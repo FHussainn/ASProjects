acd139c655f6ec8c707c67976624ba604d0d78e9
localadmin <localadmin@debian12.futureprogress.com>
2014-12-05 10:11:37 +0000

    third commit

---
+++ recipe.rb
@@ -0,0 +1,8 @@
+print "How many people will be eating?"
+people = gets.chomp.to_i
+
+pastabows = 50
+milk = 125
+cheese = 32.5
+
+puts "You need " + (people*pastabows).to_s + "g pastebows and " + (people*milk).to_s + "ml milk and " + (people*cheese).to_s + "g cheese"
